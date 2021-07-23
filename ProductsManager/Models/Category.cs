379f64aeb4a359912f640b9dfcda2cf579a15af6
Yves Jaques <ymoraisjaques@gmail.com>
2021-07-23 14:11:39 -0600

    Refactor: Category fields restrictions

--- ProductsManager/Models/Category.cs
+++ ProductsManager/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace ProductsManager.Models
     public class Category
     {
         public int Id { get; set; }
+        [Display(Name = "Description")]
+        [Required(ErrorMessage = "Required field")]
         public string Description { get; set; }
         
         public List<Product> Products { get; set; }
