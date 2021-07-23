7eae93b73aa6fb4bfae74576eaac8be1b4cc5876
Yves Jaques <ymoraisjaques@gmail.com>
2021-07-23 14:11:55 -0600

    Refactor: Product fields restrictions

--- ProductsManager/Models/Product.cs
+++ ProductsManager/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,10 @@ namespace ProductsManager.Models
     public class Product
     {
         public int Id { get; set; }
+        [Display(Name = "Description")]
 
         public string Description { get; set; }
+        [Range(1, 10, ErrorMessage = "Input value must be between 1 and 10")]
 
         public int Quantity { get; set; }
 
